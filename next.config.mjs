556d565d72928424d4eda7fcf4bc3e2f0c0713a2
smartgru <smart.gru.dev@gmail.com>
2026-02-10 21:27:07 -0500

    Update next config for pdfjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["pdfjs-dist"]
+    serverComponentsExternalPackages: ["pdfjs-dist"],
+    outputFileTracingIncludes: {
+      "/api/upload": [
+        "./node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs",
+        "./node_modules/pdfjs-dist/legacy/build/pdf.worker.min.mjs",
+        "./node_modules/pdfjs-dist/build/pdf.worker.mjs",
+        "./node_modules/pdfjs-dist/build/pdf.worker.min.mjs"
+      ]
+    }
   },
   eslint: {
     ignoreDuringBuilds: true
